d031ee89a19750b0d888d40f085a706aa328843c
Jassi Bacha <klikster@gmail.com>
2023-11-16 16:33:53 -0800

    Pt2 unroll done.

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,32 @@
+/**
+ * Unrolls a square array by extracting and concatenating its outer layer in a clockwise direction.
+ */
 function unroll(squareArray) {
+    // Check if the array is a valid array or if the length has become 0
+    if (!Array.isArray(squareArray) || squareArray.length === 0) {
+        return [];
+    }
 
+    // Extract the top row
+    const top = squareArray.shift();
+
+    // Extract the right column
+    const right = squareArray.map((arr) => arr.pop());
+
+    // Extract the bottom row
+    let bottom = [];
+    if (squareArray.length > 0) {
+        bottom = squareArray.pop().reverse();
+    }
+
+    // Extract the left column
+    const left = squareArray.map((arr) => arr.shift()).reverse();
+
+    // Combine the extracted parts
+    const outerLayer = [...top, ...right, ...bottom, ...left];
+
+    // Recursively unroll the remaining inner square array
+    return [...outerLayer, ...unroll(squareArray)];
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,59 @@
-const unroll = require("./unroll");
+const unroll = require('./unroll');
 
-describe("#unroll", function () {
+describe('#unroll', function () {
+    it('is a function', function () {
+        expect(typeof unroll).toEqual('function');
+    });
 
-  it("is a function", function () {
-    expect(typeof unroll).toEqual("function");
-  });
+    it('unrolls a 2x2 matrix correctly', function () {
+        const square = [
+            [1, 2],
+            [3, 4],
+        ];
+        expect(unroll(square)).toEqual([1, 2, 4, 3]);
+    });
 
+    it('unrolls a 4x4 matrix correctly', function () {
+        const square = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ];
+        expect(unroll(square)).toEqual([
+            1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10,
+        ]);
+    });
+
+    it('unrolls a 3x3 matrix correctly', function () {
+        const square = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+        expect(unroll(square)).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+    });
+
+    it('returns an empty array for an empty matrix', function () {
+        expect(unroll([])).toEqual([]);
+    });
+
+    it('unrolls a 1x1 matrix correctly', function () {
+        const square = [[1]];
+        expect(unroll(square)).toEqual([1]);
+    });
+
+    it('handles a non-square matrix appropriately', function () {
+        const notSquare = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+        ];
+
+        expect(unroll(notSquare)).toEqual([
+            1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7,
+        ]);
+    });
+
+    // Add more tests if needed
 });
